8c6efcab748ad6f6ca5610bea1cc0c93ef5e7c2f
Nowidas <lukaszkrupczak@gmail.com>
2023-11-05 21:18:36 +0100

    Alter max_digits for transactions values

--- backend/app/models.py
+++ backend/app/models.py
@@ -9,10 +9,13 @@ class Category(models.Model):
     def __str__(self):
         return str(self.custom_name)
 
+
 class Account(models.Model):
     account_id = models.CharField(max_length=200, unique=True)
+
     def __str__(self):
         return f"{self.account_id}"
+
 
 class Agreements(models.Model):
     agreement_id = models.CharField(max_length=200, unique=True)
@@ -39,22 +42,26 @@ class Transactions(models.Model):
         max_length=200, unique=True
     )  # account_id:transaction_id
     account = models.ForeignKey(Account, on_delete=models.SET_NULL, null=True)
-    amount = models.DecimalField(max_digits=6, decimal_places=2)
+    amount = models.DecimalField(max_digits=8, decimal_places=2)
     value_date = models.DateField()
     description = models.CharField(max_length=500)  # remittanceInformationUnstructured
-    balance_after = models.DecimalField(max_digits=6, decimal_places=2)
+    balance_after = models.DecimalField(max_digits=10, decimal_places=2)
     debtor_name = models.CharField(max_length=200, null=True, blank=True)
     debtor_account = models.CharField(max_length=200, null=True, blank=True)
     creditor_name = models.CharField(max_length=200, null=True, blank=True)
     creditor_account = models.CharField(max_length=200, null=True, blank=True)
     currency = models.CharField(max_length=50)
-    
+
     def __str__(self) -> str:
-        return f"{self.amount}{self.currency} - {self.description} ({self.transaction_id})"
+        return (
+            f"{self.amount}{self.currency} - {self.description} ({self.transaction_id})"
+        )
+
 
 class Task(models.Model):
     status = models.CharField(default="Working")
     date_done = models.DateTimeField(default=datetime.now, null=True, blank=True)
     affected_account = models.ManyToManyField(Agreements, null=True)
+
     def __str__(self) -> str:
         return f"{self.status}"
