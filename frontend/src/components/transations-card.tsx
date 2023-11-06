22035757fb155cf3fb5832a6e1bdf7a7f2bacf23
Nowidas <lukaszkrupczak@gmail.com>
2023-11-06 14:19:39 +0100

    refetch data button

--- frontend/src/components/transations-card.tsx
+++ frontend/src/components/transations-card.tsx
@@ -3,7 +3,7 @@ import axiosSesion from "./helpers/sesioninterceptor"
 import { Transactions, columns } from "./transactions-column"
 import { DataTable } from "./transactions-data-table"
 import { useEffect, useMemo, useState } from "react"
-import { useQuery } from "@tanstack/react-query"
+import { useQuery, useQueryClient } from "@tanstack/react-query"
 import { PaginationState } from "@tanstack/react-table"
 import {
   Card,
@@ -13,8 +13,18 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
+import { Button } from "./ui/button"
+import { RefreshCcw } from "lucide-react"
 
 export const TransactionsCard = () => {
+  const queryClient = useQueryClient()
+  const [fetching, setFetching] = useState(false)
+  const refetch = async () => {
+    setFetching(true)
+    await queryClient.refetchQueries({ queryKey: ['transactions'], type: 'active', })
+    setFetching(false)
+  }
+
   return (
     <>
       <Card className="w-[1000px]">
@@ -23,6 +33,7 @@ export const TransactionsCard = () => {
             <CardTitle className="text-3xl font-bold tracking-tight">Transactions</CardTitle>
             <CardDescription className="text-sm text-muted-foreground">All fetched transactions from connected accounts</CardDescription>
           </div>
+          <Button disabled={fetching} onClick={refetch} variant="outline" className="p-1 w-10 h-10"><RefreshCcw /></Button>
         </CardHeader>
         <CardContent>
           <div className="flex flex-col space-y-2 m-4">
